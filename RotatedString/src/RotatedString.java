ccad37027da61cca4dfc93d12f8ed10f4777d5e2
Oscar <oscar.lukans@outlook.com>
2014-11-06 14:07:49 -0500

    Update RotatedString.java

--- RotatedString/src/RotatedString.java
+++ RotatedString/src/RotatedString.java
@@ -1,22 +1,14 @@
 public class RotatedString {
 
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 		System.out.println(isSubstring("goodg", "oodgg"));
-
 	}
 	
-	public static boolean isSubstring(String s1,String s2)
-	{
+	public static boolean isSubstring(String s1,String s2) {
 		int length = s1.length();
 		
-		if(length == s2.length() && length>0)
-		{
-			if((s1 + s2).contains(s2))
-				return true;
-		}
-		
-		return false;
+		return (length == s2.length() && 
+		        length > 0 && 
+		        (s1 + s2).contains(s2));
 	}
-
 }
